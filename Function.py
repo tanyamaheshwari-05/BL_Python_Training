0dac546f049c26b28575e0c023745c16d5d34028
tanyamaheshwari-05 <tanya47887@gmail.com>
2026-03-05 22:01:59 +0530

    Learn Functions in python

---
+++ Function.py
@@ -0,0 +1,20 @@
+# Function without parameter
+def fun1():
+    print("Hello ")
+fun1()
+
+# Fuction with parameter
+def fun(name):
+    print(name)
+fun("Tanya")
+
+ # Function without parameter and with return type
+def fun2():
+    return "Tanya"
+print(fun2())
+
+# function with parameter and return type
+def fun3(name):
+    return name
+print("Greet" , fun3("Tanya"))
+
