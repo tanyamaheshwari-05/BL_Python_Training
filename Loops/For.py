937e8c621a5c947d913a06b85b162dfd8007a9cf
tanyamaheshwari-05 <tanya47887@gmail.com>
2026-03-06 18:33:45 +0530

    Practice of loops in python

---
+++ Loops/For.py
@@ -0,0 +1,8 @@
+# WAP to find the factorial of first n numbers. (using for)
+
+n = int(input("Enter number :"))
+fact=1
+for i in range(1,n+1):
+    fact*=i
+   
+print(fact)
---
+++ Loops/While.py
@@ -0,0 +1,8 @@
+# WAP to find the sum of first n numbers. (using while)
+
+n = int(input("Enter number :"))
+sum=0
+while(n>=0):
+    sum+=n
+    n=n-1
+print(sum)
