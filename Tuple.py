2d5f1399d15d5c723c0fd782a405709a5ea8fd97
tanyamaheshwari-05 <tanya47887@gmail.com>
2026-03-06 13:23:31 +0530

    Practice of tuple in python

---
+++ Tuple.py
@@ -0,0 +1,19 @@
+
+# Slicing in tuple 
+tup = (1,3,2,5,4)
+print(tup)
+print(tup[1:4])
+print(tup[:4])
+
+
+tup1 =(1,) #for single value tuple its important for put commmas 
+print(tup1)
+
+# Methods in tuple
+tupl = (10,30,20,30,30)
+print(tupl.index(30)) # gives first occurence of element
+print(tupl.count(30)) # gives total occurence of that element.
+
+
+
+
