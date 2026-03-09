0cd882fb9560d984335130813e2c2cc84186c4eb
tanyamaheshwari-05 <tanya47887@gmail.com>
2026-03-09 12:55:36 +0530

    Added code of Distance from origin

---
+++ Functional/Distance.py
@@ -0,0 +1,8 @@
+import math
+
+x = int(input("Enter distance from x-axis : "))
+y = int(input("Enter distance from y-axis : "))
+
+distance= math.sqrt(x*x +y*y)
+
+print("Distance from origin(0,0) : ",distance)
