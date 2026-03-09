f1e0cacc24301e8fa9da6232294d86fd677ae2f1
tanyamaheshwari-05 <tanya47887@gmail.com>
2026-03-09 11:00:08 +0530

    Practice of Abstraction in python

---
+++ Abtsraction.py
@@ -0,0 +1,16 @@
+from abc import ABC , abstractmethod
+class Animal(ABC):
+    @abstractmethod
+    def sound(self):
+        pass
+class Dog(Animal):
+    def sound(self):
+        print("Bark")
+class Cat(Animal):
+    def sound(self):
+        print("Meow")
+d= Dog()
+d.sound()
+
+c= Cat()
+c.sound()
